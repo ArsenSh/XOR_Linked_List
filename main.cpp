f017cd38d50e61e1f1eb3f9024cef70140d389dc
Arsen Shahinyan <102800732+ArsenSh@users.noreply.github.com>
2022-06-15 17:47:47 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "list_header.h"
+
+int main()
+{
+    List<int> list1{1,2,3,4,5,6,7,8,9};
+    List<int> list2(10);
+    List<int> list3;
+
+    
+    list1.print_forward();
+    list2.print_forward();
+    list3.print_forward();
+    
+    std::cout << list1 << std::endl;
+    
+    return 0;
+}
+
+
+// cout operator and traverse
